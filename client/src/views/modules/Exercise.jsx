6aa1a130bc439ef60a9964f2ff54b3f7bd9ebb25
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-08-15 16:35:10 -0300

    message in exercise module

--- client/src/views/modules/Exercise.jsx
+++ client/src/views/modules/Exercise.jsx
@@ -4,7 +4,8 @@ export default function Exercises(props) {
     return (
         <>
             <div className={props.visible ? `block` : `hidden`}>
-                <h1>Exercises</h1>
+                <h1 className="text-center text-2xl font-medium">Nenhum Exercício Postado</h1>
+                <p className="text-center italic">Todos os exercícios aparecerão aqui</p>
             </div>
         </>
     )
