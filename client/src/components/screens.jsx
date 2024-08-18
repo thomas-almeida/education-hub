5e2fb2a08fcfe2c1002e8de06d8710ea72747822
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2024-08-17 22:12:22 -0300

    adding userdata to Class Component

--- client/src/components/screens.jsx
+++ client/src/components/screens.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 
+//import { useEffect, useState } from "react"
 import Class from "../views/modules/Class"
 import Exercises from "../views/modules/Exercise"
 import Menu from "../views/modules/Menu"
@@ -8,8 +10,8 @@ export default function Screens({ activeScreen, userData, setActiveScreen }) {
 
     return (
         <>
-            {activeScreen === 'menu' && <Menu visible={true} userData={userData} setActiveScreen={setActiveScreen}/>}
-            {activeScreen === 'class' && <Class visible={true} />}
+            {activeScreen === 'menu' && <Menu visible={true} userData={userData} setActiveScreen={setActiveScreen} />}
+            {activeScreen === 'class' && <Class visible={true} userData={userData} />}
             {activeScreen === 'exercises' && <Exercises visible={true} />}
         </>
     )
