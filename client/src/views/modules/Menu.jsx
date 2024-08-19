06e5791259a2b810c0a3abeb67ab95919c65e4e8
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-08-19 20:03:34 -0300

    Numeric Timestamp

--- client/src/views/modules/Menu.jsx
+++ client/src/views/modules/Menu.jsx
@@ -25,7 +25,9 @@ export default function Menu(
 
     function formatDate(timestamp) {
 
-        const date = new Date(timestamp)
+        const numericTimeStamp = Number(timestamp)
+
+        const date = new Date(numericTimeStamp)
         const day = String(date.getDate()).padStart(2, '0')
         const month = String(date.getMonth() + 1).padStart(2, '0')
         const year = String(date.getFullYear()).slice(-2)
