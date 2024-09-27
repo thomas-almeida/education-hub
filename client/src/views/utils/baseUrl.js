e00d5d8f6e28dbf52387fe284bf7da11f1a03b22
thomas-almeida <contato.thomasalmeidard@gmail.com>
2024-09-27 18:37:04 -0300

    new prod url

--- client/src/views/utils/baseUrl.js
+++ client/src/views/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const localUrl = 'http://localhost:3004'
-const productionUrl = 'https://rodent-moved-morally.ngrok-free.app'
+const productionUrl = 'https://rodent-moved-morally.ngrok-free.app/backend2/'
 
 export default {
   localUrl,
